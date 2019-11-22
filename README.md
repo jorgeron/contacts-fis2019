c33fa63d907063038253da79ad793a8d6632b1e1
jorgeron <jorgeron1993@hotmail.com>
2019-11-22 15:41:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# contact-fis2019
+
+Código de ejemplo de la asignatura FIS 2019-2020
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var bodyParser = require('body-parser')
+
+var BASE_API_PATH = "/api/v1"
+
+var contacts = [
+    {"name" : "Peter", "phone" : "678678687"},
+    {"name" : "John", "phone" : "777777777"}
+]
+
+app.use(bodyParser.json())
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get(BASE_API_PATH + "/contacts", (req, res) => {
+    console.log(Date() + " - GET /contacts")
+    res.send(contacts)
+})
+
+app.post(BASE_API_PATH + "/contacts", (req, res) => {
+    console.log(Date() + "- POST /contacts")
+    var contact = req.body;
+    contacts.push(contact)
+    res.sendStatus(201)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
